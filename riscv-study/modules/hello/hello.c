fab116aa70e11aa5033df7f57608ca822f0b2b10
JaeJoon Jung <rgbi3307@gmail.com>
2023-02-27 13:28:53 +0900

    Add riscv-study/modules/timer/*
            modified:   riscv-study/modules/hello/Makefile
            modified:   riscv-study/modules/hello/hello.c
            new file:   riscv-study/modules/hello/hello.i
            new file:   riscv-study/modules/hello/hello.s
            new file:   riscv-study/modules/timer/Makefile
            new file:   riscv-study/modules/timer/my-timer.c
            new file:   riscv-study/modules/timer/my-timer.i
            new file:   riscv-study/modules/timer/my-timer.s
    
    Signed-off-by: JaeJoon Jung <rgbi3307@naver.com>

--- riscv-study/modules/hello/hello.c
+++ riscv-study/modules/hello/hello.c
@@ -1,15 +1,29 @@
-// hello.c
+// SPDX-License-Identifier: GPL-2.0-only
+/*
+ *      hello.c
+ *
+ *      Jung-JaeJoon <rgbi3307@gmail.com>
+ */
+
+#define pr_fmt(fmt)     "MY-MODULE: " fmt
+
 #include <linux/module.h>
 #include <linux/kernel.h>
+#include <linux/init.h>
 
-int init_module(void) {
-    printk(KERN_INFO "Hello world.\n");
-    return 0;
+static int __init example_init(void)
+{
+        pr_info("Hello world.\n");
+        return 0;
 }
 
-void cleanup_module(void) {
-    printk(KERN_INFO "Goodbye world.\n");
+static void __exit example_exit(void)
+{
+        pr_info("Goodbye world.\n");
 }
+
+module_init(example_init);                                                      
+module_exit(example_exit);
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Linux");
