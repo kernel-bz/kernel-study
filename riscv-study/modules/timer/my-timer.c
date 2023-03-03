91dead99bc7fa900cffef3a0d15bad510c680c2a
JaeJoon Jung <rgbi3307@gmail.com>
2023-03-03 17:26:39 +0900

    Modified riscv-study/modules/timer
    On branch CV20230303
    Changes to be committed:
    	modified:   riscv-study/modules/timer/my-timer.c
    	deleted:    riscv-study/modules/timer/my-timer.i
    	deleted:    riscv-study/modules/timer/my-timer.s
    
    Signed-off-by: JaeJoon Jung <rgbi3307@naver.com>

--- riscv-study/modules/timer/my-timer.c
+++ riscv-study/modules/timer/my-timer.c
@@ -14,23 +14,64 @@
 
 #define TIMEOUT 5000    //milliseconds to expire
 
-static struct timer_list my_timer;
+static struct timer_list my_timer1;
+static struct timer_list my_timer2;
 static unsigned int cnt = 0;
 
-void my_timer_callback(struct timer_list *timer)
+void my_timer1_callback(struct timer_list *timer)
 {
-        pr_info("Called(%u): my_timer_callback().\n", cnt++);
+        pr_info("Called(%u): my_timer1_callback().\n", cnt++);
 
-        mod_timer(&my_timer, jiffies + msecs_to_jiffies(TIMEOUT));
+        mod_timer(&my_timer1, jiffies + msecs_to_jiffies(TIMEOUT));
+}
+
+void my_timer2_callback(struct timer_list *timer)
+{
+        pr_info("Called(%u): my_timer2_callback().\n", cnt++);
+
+        mod_timer(&my_timer2, jiffies + msecs_to_jiffies(TIMEOUT));
+
+        if (cnt > 3) {
+                int pending;
+
+                pending = timer_pending(&my_timer1);
+                pr_info("pending: %d\n", pending);
+
+                //del_timer_sync(&my_timer1);
+                timer_delete_sync(&my_timer1);
+
+                //timer->func = NULL (shutdown)
+                //timer_shutdown_sync(&my_timer1);
+
+                pending = timer_pending(&my_timer1);
+                pr_info("pending: %d\n", pending);
+
+                mod_timer(&my_timer1, jiffies + msecs_to_jiffies(TIMEOUT));
+
+                pending = timer_pending(&my_timer1);
+                pr_info("pending: %d\n", pending);
+
+                cnt = 0;
+        }
 }
 
 static int __init my_timer_init(void)
 {
+        int pending;
+
         pr_info("init_module()...\n");
 
-        timer_setup(&my_timer, my_timer_callback, 0);
+        timer_setup(&my_timer1, my_timer1_callback, 0);
+        timer_setup(&my_timer2, my_timer2_callback, 0);
 
-        mod_timer(&my_timer, jiffies + msecs_to_jiffies(TIMEOUT));
+        pending = timer_pending(&my_timer1);
+        pr_info("pending: %d\n", pending);      //0
+
+        mod_timer(&my_timer1, jiffies + msecs_to_jiffies(TIMEOUT));
+        mod_timer(&my_timer2, jiffies + msecs_to_jiffies(TIMEOUT));
+
+        pending = timer_pending(&my_timer1);
+        pr_info("pending: %d\n", pending);     //1
 
         pr_info("Done: my timer setup.\n");
 
@@ -41,8 +82,10 @@ static void __exit my_timer_exit(void)
 {
         pr_info("cleanup_module()...\n");
 
-        //del_timer(&my_timer);
-        del_timer_sync(&my_timer);
+        //del_timer_sync(&my_timer1);
+        timer_shutdown_sync(&my_timer1);
+        //del_timer_sync(&my_timer2);
+        timer_shutdown_sync(&my_timer2);
         
         pr_info("Done: my timer delete.\n");
 }
